28591ec352387248d9cc0101dafc15e7ae91b49d
Astraea11 <maricar.seron.solamo@gmail.com>
2024-12-04 23:16:53 +0800

    Delete mysqli_connect.php

--- mysqli_connect.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$dbcon = @mysqli_connect('localhost','marsolamo','marsolamo', 'members_solamo')
-OR die('Could not to the MySQL Server: '. mysqli_connect_error());
-mysqli_set_charset($dbcon, 'utf8');
